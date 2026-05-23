a6d6443f5e9a5116d3d8e3f685a6f57d1cea253d
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-23 22:54:39 +0530

    Started Learning React

---
+++ 02_0_FRONTEND_DEVELOPMENT/04_0_REACT/01_1_basics_vite/src/App.jsx
@@ -0,0 +1,26 @@
+// import yt from "./yt"
+
+
+// function App() {
+
+//   return(
+//     <>
+//     <h1>Vite React App</h1>
+//     <h1>Vite React App</h1>
+//     <yt/>
+//     {/* we can return one element but here I am returning two because I am treating 2 elemnts as one by inserting them into div tag */}
+//     </>
+//   )
+// }
+import Yt from "./yt"
+
+function App() {
+  return (
+    <>
+      <h1>Vite React App</h1>
+      <h1>Vite React App</h1>
+      <Yt/>
+    </>
+  )
+}
+export default App
---
+++ 02_0_FRONTEND_DEVELOPMENT/04_0_REACT/01_1_basics_vite/src/yt.jsx
@@ -0,0 +1,5 @@
+function Yt() {
+  return <h1>Youtube App</h1>
+}
+
+export default Yt
