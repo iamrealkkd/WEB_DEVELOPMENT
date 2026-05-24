a360739c9bd2ae2ac0e45d9cd51bd242692f273e
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-24 16:42:09 +0530

    Tailwind Linking

---
+++ 02_0_FRONTEND_DEVELOPMENT/04_0_REACT/04_0_tailwindprops/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-red-500 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
