e2b213762b65abd09d4b16ae9970bbcb64188a22
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-26 17:28:13 +0530

    JSX Rules

---
+++ 02_0_FRONTEND_DEVELOPMENT/04_0_REACT/09_0_jsx_rules/src/App.jsx
@@ -0,0 +1,22 @@
+import JSXRules from "./components/JSXRules";
+
+const App = () => {
+  return (
+    // <section id="section">
+    //   <h1>My Website</h1>
+    //   <article>
+    //     <h2>Welcome To React</h2>
+    //     <p className="text">Paragraph Content</p>
+    //   </article>
+    //   <form>
+    //     <label htmlFor="name">Name </label>
+    //     <input type="text" placeholder="Enter your name" />
+    //   </form>
+    // </section>
+    <div>
+      <JSXRules/>
+    </div>
+  );
+};
+
+export default App;
---
+++ 02_0_FRONTEND_DEVELOPMENT/04_0_REACT/09_0_jsx_rules/src/components/JSXRules.jsx
@@ -0,0 +1,17 @@
+const JSXRules = () => {
+  return (
+    <div>
+      <h1 style={{marginLeft: "20px"}}>JSX Rules</h1>
+      <ul>
+        <li>JSX must return a single parent element</li>
+        <li>JSX elements must be properly closed</li>
+        <li>
+          JSX attributes are written using camelCase(e.g. className instead of
+          class)
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default JSXRules;
