a62422259909b7185505285074a4999e386d52fc
iamrealkkd <kkrishna.bga@gmail.com>
2026-09-12 19:23:49 +0530

    Array Types

---
+++ 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/04_0_arrays.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [1, 2, 3, 4];
+const names: Array<string> = ["Alice", "Bob", "Charlie"];
+const items: string[] = [];
+items.push("Keyboard");
+console.log(items);
+
+const numList: number[] = [];
+numList.push(20);
+console.log(numList);
