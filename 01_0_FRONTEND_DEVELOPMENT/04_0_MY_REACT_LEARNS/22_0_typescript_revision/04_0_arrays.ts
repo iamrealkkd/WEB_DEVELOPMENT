7d908435caf42fdde4d068ec73dc9388cf3f1316
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-14 21:55:36 +0530

    New 2d Matrix

--- 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/04_0_arrays.ts
+++ 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/04_0_arrays.ts
@@ -7,3 +7,13 @@ console.log(items);
 const numList: number[] = [];
 numList.push(20);
 console.log(numList);
+
+//multidimensional array
+const matrix: number[][] = [
+        [1,2],
+        [1,6],
+];
+
+for(const row of matrix){
+    console.log(row);
+}
