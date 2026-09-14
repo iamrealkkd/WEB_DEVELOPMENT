28370d2df3644258ad2461e1f74532e8de31b04b
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-14 22:36:07 +0530

    Objects

---
+++ 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/05_0_objects.ts
@@ -0,0 +1,20 @@
+const person: { firstName: string; lastName: string; age: number } = {
+  firstName: "Krishna",
+  lastName: "Dutta",
+  age: 30,
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+
+//using objects as function return value
+function printUser(): { name: string; age: number; location: string } {
+    return {
+        name: "Alex",
+        age: 19,
+        location: "USA",
+    };
+}
+console.log(printUser());
+const rx = printUser();
+console.log(rx);
