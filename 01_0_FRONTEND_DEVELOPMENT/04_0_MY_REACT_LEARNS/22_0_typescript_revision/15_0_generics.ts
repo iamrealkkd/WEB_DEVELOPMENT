13758616d1367034f265a7a0e07cd8d5e322a3c1
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-19 07:32:24 +0530

    Generic Added

--- 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/15_0_generics.ts
+++ 01_0_FRONTEND_DEVELOPMENT/04_0_MY_REACT_LEARNS/22_0_typescript_revision/15_0_generics.ts
@@ -53,7 +53,7 @@ console.log(isLoggedIn);
 // -----------------------------------------
 function uniqueDataTypesFunc<Type>(
   item: Type,
-  defaultValue: Type
+  defaultValue: Type,
 ): [Type, Type] {
   return [item, defaultValue];
 }
@@ -62,3 +62,37 @@ const um = uniqueDataTypesFunc<number>(10, 20);
 const tr = uniqueDataTypesFunc<string>("hello", "world");
 console.log(um);
 console.log(tr);
+
+function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
+  return array.filter((item) => condition(item));
+}
+
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
+
+console.log(evenNumbers);
+
+const stringArr = ["apple", "banana", "cherry", "date"];
+
+const shortWords = filterArray<string>(stringArr, (word) => word.length < 6);
+
+console.log(shortWords);
+
+interface Fruit {
+  name: string;
+  color: string;
+}
+
+const fruitArray: Fruit[] = [
+  { name: "Apple", color: "Red" },
+  { name: "Banana", color: "Yellow" },
+  { name: "Cherry", color: "Red" },
+];
+
+const redFruits = filterArray<Fruit>(
+  fruitArray,
+  (fruit) => fruit.color === "Red",
+);
+
+console.log(redFruits);
